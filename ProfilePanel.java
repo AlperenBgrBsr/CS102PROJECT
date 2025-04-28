8e6a6c03d7d001a5351fbd10b6754d2b7d118e93
AlperKabal <alperkabal14@gmail.com>
2025-04-29 01:33:56 +0300

    Profile Panel Update
    Both own and others profile page is done without the database

--- ProfilePanel.java
+++ ProfilePanel.java
@@ -1,8 +1,11 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.concurrent.Flow;
 
 import javax.imageio.ImageIO;
@@ -21,6 +24,7 @@ public class ProfilePanel extends JPanel{
     private Image fullStar;
     private String username;
     private User currentUser;
+    private int ratingForAddRatingFrame;
 
   
 
@@ -135,8 +139,7 @@ public class ProfilePanel extends JPanel{
             reviewArea.setFocusable(false);
             reviewArea.setFont(new Font("Arial",Font.PLAIN,15));
             for (int i = 1; i <= 100; i++){
-                reviewArea.append("\n");
-                reviewArea.append("User" + i + ": BLAH BLAH BLAH BLAAH \n");
+                reviewArea.setText("\n" + " User" + i +": BLAH BLAH BLAH BLAH" + "\n" + reviewArea.getText());
             }
             
 
@@ -353,6 +356,322 @@ public class ProfilePanel extends JPanel{
         //Other Profile
         else{
             
+            //Email Label
+            JLabel emailLabel = new JLabel("abcdefg@ug.bilkent.edu.tr"); //Change it with database
+            emailLabel.setBounds(280,165,200+"abcdefg@ug.bilkent.edu.tr".length()*5,20);//Change it with database
+            emailLabel.setFont(new Font("Arias", Font.BOLD, 15));
+
+            //Username Label
+            JLabel usernameLabel = new JLabel(username);
+            usernameLabel.setBounds(160 - username.length()*2,250,100+username.length()*5,30);
+            usernameLabel.setFont(new Font("Arias", Font.BOLD, 18));
+
+            //Status Pic
+
+            ImageIcon homeIcon = new ImageIcon("homeIcon.png");
+            Image homeIconImage = homeIcon.getImage();
+            homeIconImage = homeIconImage.getScaledInstance(60, 40, java.awt.Image.SCALE_SMOOTH);
+            ImageIcon newHomeIcon = new ImageIcon(homeIconImage);
+
+            ImageIcon awayIcon = new ImageIcon("awayIcon.png");
+            Image awayIconImage = awayIcon.getImage();
+            awayIconImage = awayIconImage.getScaledInstance(60, 40, java.awt.Image.SCALE_SMOOTH);
+            ImageIcon newAwayIcon = new ImageIcon(awayIconImage);
+
+            JLabel statusPicture = new JLabel();
+            boolean availability = true;
+            if ( availability ){ //from database check username's availability
+                statusPicture = new JLabel(newHomeIcon);
+            }
+            else{
+                statusPicture = new JLabel(newAwayIcon);
+            }
+            statusPicture.setOpaque(true);
+            
+            
+            
+            statusPicture.setBounds(320,120, 60,40);
+            statusPicture.setFocusable(false);
+
+            //Rating
+
+            JLabel ratingLabel = new JLabel();
+            String formattedRating = String.format("%.2f",2.762432);
+            ratingLabel.setText(formattedRating);
+            ratingLabel.setFont(new Font("Arial Black",Font.BOLD, 35));
+            ratingLabel.setBounds(300,272,100,100);
+
+            JButton ratingCountButton = new JButton("X Ratings"); 
+            ratingCountButton.setBorder(new LineBorder(Color.BLACK,1));
+            ratingCountButton.setBounds(160,370,120,40);
+            ratingCountButton.setFont(new Font("Arial", Font.BOLD, 20));
+            ratingCountButton.setFocusable(false);  
+            ratingCountButton.setBackground(Color.white);
+            ratingCountButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog(null, "Directing...", "Title", JOptionPane.INFORMATION_MESSAGE);
+                }
+                
+            });
+
+            JButton addRatingButton = new JButton("Add Rating");
+            addRatingButton.setBorder(new LineBorder(Color.BLACK,1));
+            addRatingButton.setBounds(290,370,120,40);
+            addRatingButton.setFont(new Font("Arial", Font.BOLD, 20));
+            addRatingButton.setFocusable(false);  
+            addRatingButton.setBackground(Color.white);
+            addRatingButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    
+                    ratingForAddRatingFrame = 0;
+                    JFrame addRatingFrame = new JFrame("Add Rating");
+                    JPanel addRatingPanel = new JPanel(){
+                        
+                        protected void paintComponent(Graphics g2){
+                            super.paintComponent(g2);
+                            for (int i = 0; i < 5; i++){
+                                g2.drawImage(emptyStar, 10 + 90*i, 60, 90, 90, this);
+                            }
+                            for (int i = 0; i < ratingForAddRatingFrame; i++){
+                                g2.drawImage(fullStar, 10 + 90*i, 60, 90, 90, this);
+                            }
+
+                        }
+
+                    };
+                    
+                    addRatingPanel.addMouseListener(new MouseListener() {
+
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+            
+                            if ( e.getY() <= 150 && e.getY() >= 60 ){
+                                if ( e.getX() >= 0 && e.getX() <= 100 ){
+                                    ratingForAddRatingFrame = 1;
+
+                                }
+                                else if ( e.getX() <= 190 ){
+                                    ratingForAddRatingFrame = 2;
+                                }
+                                else if ( e.getX() <= 280 ){
+                                    ratingForAddRatingFrame = 3;
+                                }
+                                else if ( e.getX() <= 370 ){
+                                    ratingForAddRatingFrame = 4;
+                                }
+                                else if ( e.getX() <= 460 ){
+                                    ratingForAddRatingFrame = 5;
+                                }
+                                addRatingPanel.repaint();
+                            }
+                            
+                        }
+                        @Override
+                        public void mousePressed(MouseEvent e) {}
+                        @Override
+                        public void mouseReleased(MouseEvent e) {}
+                        @Override
+                        public void mouseEntered(MouseEvent e) {}
+                        @Override
+                        public void mouseExited(MouseEvent e) {}
+                        
+                    });
+
+                    JButton resetButton = new JButton("Reset");
+                    resetButton.setFocusable(false);
+                    resetButton.setBorder(new LineBorder(Color.black,1));
+                    resetButton.setBackground(Color.red);
+                    resetButton.setForeground(Color.white);
+                    resetButton.addActionListener(new ActionListener() {
+
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            ratingForAddRatingFrame = 0;
+                            addRatingPanel.repaint();
+                        }
+                        
+                    });
+                    resetButton.setBounds(60,180,150,50);
+                    resetButton.setFont(new Font("Arial", Font.BOLD, 20));
+
+                    JButton sendRating = new JButton("Send Rating");
+                    sendRating.setFocusable(false);
+                    sendRating.setBorder(new LineBorder(Color.black,1));
+                    sendRating.setBackground(Color.red);
+                    sendRating.setForeground(Color.white);
+                    sendRating.addActionListener(new ActionListener() {
+
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            ImageIcon fullstarIcon = new ImageIcon(fullStar);
+                            addRatingFrame.dispose();
+                            JOptionPane.showMessageDialog(null, "You have rated " + username + " " + ratingForAddRatingFrame + " stars!", "USER RATING", JOptionPane.INFORMATION_MESSAGE, fullstarIcon);
+                            //ADD TO DATABASE!!
+                        }
+                        
+                    });
+                    sendRating.setBounds(250,180,150,50);
+                    sendRating.setFont(new Font("Arial", Font.BOLD, 20));
+
+                    JLabel label = new JLabel("SELECT RATING FOR " + username.toUpperCase(Locale.ENGLISH ));
+                    label.setFont(new Font("Arial", Font.BOLD + Font.ITALIC, 20));
+                    label.setBounds(30,10,480,40);
+
+                    addRatingPanel.add(label);
+                    addRatingPanel.add(sendRating);
+                    addRatingPanel.add(resetButton);
+                    addRatingPanel.setLayout(null);
+                    addRatingPanel.setBackground(Color.white);
+                    addRatingPanel.setPreferredSize(new Dimension(480,250));
+                    addRatingFrame.add(addRatingPanel);
+                    addRatingFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    addRatingFrame.setResizable(false);
+                    addRatingFrame.pack();
+                    addRatingFrame.setVisible(true);
+                }
+                
+            });
+
+            //Reviews
+           
+            JTextArea reviewArea = new JTextArea();
+            reviewArea.setLineWrap(true);
+            reviewArea.setWrapStyleWord(true);
+            reviewArea.setEditable(false);
+            reviewArea.setFocusable(false);
+            reviewArea.setFont(new Font("Arial",Font.PLAIN,15));
+            for (int i = 1; i <= 100; i++){
+                reviewArea.setText("\n" + " User" + i +": BLAH BLAH BLAH BLAH" + "\n" + reviewArea.getText());
+            }
+            
+
+            JScrollPane scrollPaneForReviewArea = new JScrollPane(reviewArea);
+            scrollPaneForReviewArea.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+            scrollPaneForReviewArea.setBorder(new LineBorder(Color.BLACK,1));
+            scrollPaneForReviewArea.setBounds(30,420,500,300);
+            scrollPaneForReviewArea.setBackground(Color.white);
+            
+
+
+            JTextField reviewTextField = new JTextField(" Add Comment");
+            reviewTextField.setFont(new Font("Arial",Font.ITALIC,15));
+            reviewTextField.setBorder(new LineBorder(Color.BLACK,1));
+            reviewTextField.setBounds(30,720,440,30);
+            reviewTextField.setPreferredSize(new Dimension(440,30));
+            reviewTextField.setBackground(Color.white);
+            reviewTextField.addMouseListener(new MouseListener() { //To Empty The Text Field
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if ( reviewTextField.getText().equals(" Add Comment") ){
+                        reviewTextField.setText("");
+                    }
+                }
+                @Override
+                public void mousePressed(MouseEvent e) {}
+                @Override
+                public void mouseReleased(MouseEvent e) {}
+                @Override
+                public void mouseEntered(MouseEvent e) {}
+                @Override
+                public void mouseExited(MouseEvent e) {}
+                
+            });
+
+            JButton reviewSendButton = new JButton("Send");
+            reviewSendButton.setFont(new Font("Arial",Font.ITALIC + Font.BOLD,15));
+            reviewSendButton.setBorder(new LineBorder(Color.BLACK,1));
+            reviewSendButton.setBounds(470,720,60,30);
+            reviewSendButton.setPreferredSize(new Dimension(60,30));
+            reviewSendButton.setBackground(Color.white);
+            reviewSendButton.setFocusable(false);
+            reviewSendButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String review = reviewTextField.getText();
+                    reviewArea.setText("\n" + " " + username + ": " + review + "\n" + reviewArea.getText());
+                    reviewArea.setCaretPosition(0);
+                    //add review to database!!!!!
+                }
+                
+            });
+
+            JLabel reviewLabel = new JLabel("  X Reviews");
+            reviewLabel.setBorder(new LineBorder(Color.BLACK,1));
+            reviewLabel.setBounds(30,370,120,40);
+            reviewLabel.setFont(new Font("Arial", Font.BOLD, 20));
+
+            //Adverts Found
+
+            JButton advertsFoundButton = new JButton("X Adverts Found");
+            advertsFoundButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog(null, "Directing...", "Title", JOptionPane.INFORMATION_MESSAGE);
+                }
+                
+            });
+            advertsFoundButton.setFocusable(false);
+            advertsFoundButton.setBorder(new LineBorder(Color.BLACK,1));
+            advertsFoundButton.setBackground(Color.white);
+            advertsFoundButton.setFont(new Font("Arial",Font.BOLD,20));
+            advertsFoundButton.setBounds(700,230,200,40);
+
+            //Add to Contact Button
+
+            JButton addToContactButton = new JButton("Add to Contacts");
+            addToContactButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog(null, "Directing...", "Title", JOptionPane.INFORMATION_MESSAGE);
+                }
+                
+            });
+            addToContactButton.setFocusable(false);
+            addToContactButton.setBorder(new LineBorder(Color.BLACK,1));
+            addToContactButton.setBackground(Color.white);
+            addToContactButton.setFont(new Font("Arial",Font.BOLD,20));
+            addToContactButton.setBounds(700,500,200,40);
+
+
+            //Remove Contact Button
+
+            JButton removeContactButton = new JButton("Remove Contact");
+            removeContactButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog(null, "Directing...", "Title", JOptionPane.INFORMATION_MESSAGE);
+                }
+                
+            });
+            removeContactButton.setFocusable(false);
+            removeContactButton.setBorder(new LineBorder(Color.BLACK,1));
+            removeContactButton.setBackground(Color.white);
+            removeContactButton.setFont(new Font("Arial",Font.BOLD,20));
+            removeContactButton.setBounds(700,560,200,40);
+            
+
+
+            this.add(reviewSendButton);
+            this.add(reviewTextField);
+            this.add(statusPicture);
+            this.add(removeContactButton);
+            this.add(addToContactButton);
+            this.add(advertsFoundButton);
+            this.add(scrollPaneForReviewArea);
+            this.add(reviewLabel);
+            this.add(usernameLabel);
+            this.add(emailLabel);
+            this.add(ratingLabel);
+            this.add(ratingCountButton);
+            this.add(addRatingButton);
         }
 
 
@@ -367,31 +686,32 @@ public class ProfilePanel extends JPanel{
         for (int i = 0; i < 5; i++){
             g.drawImage(emptyStar, 60 + 45*i, 300, 45, 45, this);
         }
-        if ( currentUser.getUsername().equals(username) ){
-            double rating = currentUser.getRating();
-            int drawnStarAmount = 0;
-            while ( rating >= 1 ){
 
-                g.drawImage(fullStar, 60 + 45*drawnStarAmount, 300, 45, 45, this);
-                rating--;
-                drawnStarAmount++;
-            }
-            if ( rating >= 0.75 && rating < 1){
-                g.drawImage(threeQuarterStar, 60 + 45*drawnStarAmount, 300, 45, 45, this);
-            }
-            else if (rating >= 0.5){
-                g.drawImage(halfStar, 60 + 45*drawnStarAmount, 300, 45, 45, this);
-            }
-            else if ( rating >= 0.25 ){
-                g.drawImage(oneQuarterStar, 60 + 45*drawnStarAmount, 300, 45, 45, this);
-            }
+        double rating = 0;
+        int drawnStarAmount = 0;
+        if ( currentUser.getUsername().equals(username) ){
             
+           rating = currentUser.getRating();   
         }   
         else{
-
+            rating = 2.6; //Change it with database
         }
 
+        while ( rating >= 1 ){
 
+            g.drawImage(fullStar, 60 + 45*drawnStarAmount, 300, 45, 45, this);
+            rating--;
+            drawnStarAmount++;
+        }
+        if ( rating >= 0.75 && rating < 1){
+            g.drawImage(threeQuarterStar, 60 + 45*drawnStarAmount, 300, 45, 45, this);
+        }
+        else if (rating >= 0.5){
+            g.drawImage(halfStar, 60 + 45*drawnStarAmount, 300, 45, 45, this);
+        }
+        else if ( rating >= 0.25 ){
+            g.drawImage(oneQuarterStar, 60 + 45*drawnStarAmount, 300, 45, 45, this);
+        }
     }   
 
 
