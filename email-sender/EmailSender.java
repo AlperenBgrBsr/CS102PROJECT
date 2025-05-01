98789791d4a3749f57222f321931c5efc3df5a9e
Alperen Buğra Başar <bugra.basar@ug.bilkent.edu.tr>
2025-05-01 14:47:14 +0300

    Add files via upload

---
+++ email-sender/EmailSender.java
@@ -0,0 +1,71 @@
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+import java.util.*;
+
+public class EmailSender {
+    private final String USERNAME = "bilmartsystem@gmail.com"; 
+    private final String PASSWORD = "tkuqozkfpajskspw"; 
+    private Random random = new Random();
+
+    public void sendInformationEmail(User from, User to, Advert advert) {
+        String fromName = from.getUsername();
+        String toName = to.getUsername();
+        int advertID = advert.getId();
+        int fromID = from.getID();
+
+        Properties props = new Properties();
+        int code = random.nextInt(100000,1000001);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(USERNAME, PASSWORD);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(USERNAME));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to.getEmail()));
+            message.setSubject("Information mail");
+            message.setText("User " + fromName + " with ID "+ fromID + " has added your advert with ID " + advertID);
+            Transport.send(message);
+            System.out.println("Email sent: " + to.getUsername());
+        } catch (MessagingException e) {
+            System.err.println("Email failed to send!");
+            e.printStackTrace();
+        }
+    }
+
+    public void sendVerificationEmail(User to) {
+        Properties props = new Properties();
+        int code = random.nextInt(100000,1000001);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(USERNAME, PASSWORD);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(USERNAME));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to.getEmail()));
+            message.setSubject("Verification email");
+            message.setText("Your verification code for registration: " + code);
+            Transport.send(message);
+            System.out.println("Email sent: " + to.getUsername());
+        } catch (MessagingException e) {
+            System.err.println("Email failed to send!");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ email-sender/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        EmailSender sender = new EmailSender();
+        sender.sendVerificationEmail("bugra.basar@ug.bilkent.edu.tr");
+        
+    }
+}
