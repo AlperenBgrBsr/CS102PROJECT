6ff3fa7f380b834d2a1da10f70fbccc28d2ab8cf
AlperKabal <alperkabal14@gmail.com>
2025-05-02 16:56:55 +0300

    Update MainFile.java
    forgot to delete the databaseConnection variable

--- MainFile.java
+++ MainFile.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MainFile {
-    public static Connection databaseConnection;
+
     public static HomeScreen hm;
     public static void main(String[] args) {
         Database.createConnection();
