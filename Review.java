4e5697ed6a97e32b63095c16c62d2098f84b1e34
AlperKabal <alperkabal14@gmail.com>
2025-05-02 16:54:13 +0300

    Updated Review Class

--- Review.java
+++ Review.java
@@ -1,6 +1,3 @@
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Review {
     private String senderUsername;
@@ -13,37 +10,11 @@ public class Review {
         this.recieverUsername = recieverUsername;
         this.reviewContent = review;
     }
-    public void addToDatabase(){
-        try {
-            
-            int senderId = 0; 
-            int recieverId = 0;
-            PreparedStatement senderIdStatement = Main.databaseConnection.prepareStatement("SELECT user_id FROM users WHERE username = ?");
-            senderIdStatement.setString(1, senderUsername);
-            ResultSet senderRs = senderIdStatement.executeQuery();
-            while (senderRs.next()){
-                senderId = senderRs.getInt("user_Id");
-            }
-            PreparedStatement recieverIdStatement = Main.databaseConnection.prepareStatement("SELECT user_id FROM users WHERE username = ?");
-            recieverIdStatement.setString(1, recieverUsername);
-            ResultSet recieverRs = recieverIdStatement.executeQuery();
-            while (recieverRs.next()){
-                recieverId = recieverRs.getInt("user_Id");
-            }
-            PreparedStatement reviewToDatabaseStatement = Main.databaseConnection.prepareStatement("INSERT INTO reviews VALUES (?,?,?)");
-            reviewToDatabaseStatement.setInt(1, senderId);
-            reviewToDatabaseStatement.setInt(2, recieverId);
-            reviewToDatabaseStatement.setString(3, reviewContent);
-            reviewToDatabaseStatement.executeUpdate();
-            
-        } catch (SQLException e) {  
-            e.printStackTrace();
-        }
-    }
+    
     public String getRecieverUsername() {
         return recieverUsername;
     }
-    public String getReview() {
+    public String getReviewContent() {
         return reviewContent;
     }
     public String getSenderUsername() {
