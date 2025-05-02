7294c6a6c325a5dd268784515ff26dd21ad9b840
AlperKabal <alperkabal14@gmail.com>
2025-05-02 16:56:22 +0300

    Update MainFile.java
    changes with the database class

--- MainFile.java
+++ MainFile.java
@@ -6,16 +6,7 @@ public class MainFile {
     public static Connection databaseConnection;
     public static HomeScreen hm;
     public static void main(String[] args) {
-        createConnection();
+        Database.createConnection();
         hm = new HomeScreen();
-    }
-     private static void createConnection()  {
-        
-        try {
-            databaseConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bilmartdb", "username", "password");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        
     }
 }
