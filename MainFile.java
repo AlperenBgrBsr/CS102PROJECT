357fa0bd3f865e8ae98532e9275520b8a89350f9
Mete Alkan <mete.alkan@ug.bilkent.edu.tr>
2025-05-07 23:01:40 +0300

    Update MainFile.java

--- MainFile.java
+++ MainFile.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MainFile {
-
-   
-    public static void main(String[] args) {
+   public static User currentUserForAll = new User("Hyper10nBtw", "m.a@ug.bilmart.edu.tr","e"); //This is just for debugging purposses, can delete it or change if needed on your own
+    
+   public static void main(String[] args) {
         Database.createConnection();
         HomeScreen.hm = new HomeScreen();
     }
