a896b049765e1917ae2d4c183e94f0073336e7c6
AlperKabal <alperkabal14@gmail.com>
2025-05-11 22:03:34 +0300

    Add files via upload

--- email-sender/EmailSender.java
+++ email-sender/EmailSender.java
@@ -1,6 +1,6 @@
 import javax.mail.*;
 import javax.mail.internet.*;
-import java.util.Properties;
+
 import java.util.*;
 
 public class EmailSender {
@@ -8,14 +8,13 @@ public class EmailSender {
     private final static String PASSWORD = "tkuqozkfpajskspw"; 
     private static Random random = new Random();
 
-    public static void sendInformationEmail(User from, User to, Advert advert) {
-        String fromName = from.getUsername();
-        String toName = to.getUsername();
+    public static void sendInformationEmailForAdvert(User from, String toName, String toEmail, Advert advert) { // Bu ve aşağıdaki methodun kullanılacağı yerde ikinci bi user yok o yüzden direkt bunları almak parametre olarak daha iyi olabilir
+        String fromName = from.getUsername(); 
+
         String advertTitle = advert.getTitle();
-        String fromUsername = from.getUsername();
+        String fromEmail = from.getEmail();
 
         Properties props = new Properties();
-        int code = random.nextInt(100000,1000001);
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
@@ -23,27 +22,56 @@ public class EmailSender {
 
         Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(USERNAME, PASSWORD);
+                return new PasswordAuthentication("bilmartsystem@gmail.com", "tkuqozkfpajskspw");
             }
         });
 
         try {
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("bilmartsystem@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to.getEmail()));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
             message.setSubject("Information mail");
-            message.setText("User " + fromName + " with username "+ fromUsername + " has added your advert with title " + advertTitle);
+            message.setText("User " + fromName + " with email "+ fromEmail + " has added your advert with the title " + advertTitle);
             Transport.send(message);
-            System.out.println("Email sent: " + to.getUsername());
+            System.out.println("Email sent: " + toName);
         } catch (MessagingException e) {
             System.err.println("Email failed to send!");
             e.printStackTrace();
         }
     }
 
-    public static void sendVerificationEmail(User to) {
+    public static void sendInformationEmailForContact(User from, String toName, String toEmail) {
+        String fromName = from.getUsername();
+        String fromEmail = from.getEmail();
+
         Properties props = new Properties();
-        int code = random.nextInt(100000,1000000);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("bilmartsystem@gmail.com", "tkuqozkfpajskspw");
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("bilmartsystem@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject("Information mail");
+            message.setText("User " + fromName + " with email "+ fromEmail + " has added you to contacts");
+            Transport.send(message);
+            System.out.println("Email sent: " + toName);
+        } catch (MessagingException e) {
+            System.err.println("Email failed to send!");
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendVerificationEmail(User to, int code) {
+        Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
