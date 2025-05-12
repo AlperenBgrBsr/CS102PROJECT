6dbd066810fb73bcd3f6e9b1841950a216d1d652
AlperKabal <alperkabal14@gmail.com>
2025-05-12 23:00:11 +0300

    Add files via upload

--- MainFile.java
+++ MainFile.java
@@ -1,13 +1,13 @@
 
 
 public class MainFile {
-   public static User currentUserForAll; //This is just for debugging purposses, can delete it or change if needed on your own
-    
+    public static User currentUserForAll; //This is just for debugging purposses, can delete it or change if needed on your own
+    public static LoginScreen log;
     
     public static void main(String[] args) {
     
     Database.createConnection();
-    LoginScreen log = new LoginScreen();
+    log = new LoginScreen();
     RegisterScreen reg = new RegisterScreen(log);
     reg.setVisible(false);
     log.connect(reg);
