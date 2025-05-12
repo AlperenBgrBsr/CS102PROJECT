18a6655c4b78e3ace50eb9d84fb5b91641efc862
AlperKabal <alperkabal14@gmail.com>
2025-05-12 23:31:45 +0300

    Delete email-sender/Main.java

--- email-sender/Main.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Main {
-    public static void main(String[] args) {
-        EmailSender sender = new EmailSender();
-        sender.sendVerificationEmail("bugra.basar@ug.bilkent.edu.tr");
-        
-    }
-}
